4d47484c6576ccf1c829415968402292befbd335
RProduction <starfallproduction42@gmail.com>
2024-04-27 11:17:20 +0700

    Implement header and router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from './pages/Home';
+import Services from './pages/Services';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "services",
+        element: <Services/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
