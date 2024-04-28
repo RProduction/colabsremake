137ef84e5c8417e665dcfc4b540b25ac6f76e0cc
RProduction <starfallproduction42@gmail.com>
2024-04-28 15:46:30 +0700

    implement full screen carousel

---
+++ src/components/FullScreenCarousel.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import ImageCard from './ImageCard';
+
+import Box from '@mui/material/Box';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Autoplay} from 'swiper/modules';
+import useTheme from '@mui/material/styles/useTheme';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
+interface Props {
+  content: Cards[];
+}
+
+interface Cards {
+  title: string;
+  img: string;
+  bg: string;
+}
+
+function FullScreenCarousel(props: Props) {
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.only("xs"));
+  const isSm = useMediaQuery(theme.breakpoints.only("sm"));
+  const isUpper = useMediaQuery(theme.breakpoints.up("sm"));
+
+  let slides : number = 0;
+  if(isXs) {
+    slides = 1;
+  } else if(isSm) {
+    slides = 2;
+  } else if(isUpper) {
+    slides = 3
+  }
+
+  return(
+    <Box
+      height={200}
+      component={Swiper}
+      modules={[Autoplay]}
+      slidesPerView={slides}
+      autoplay={{delay: 2000}}
+      loop
+      sx={{
+        "& .swiper-slide": {
+          marginX: 1
+        }
+      }}
+    >
+      {
+        props.content.map((value, i) => (
+          <SwiperSlide key={`${value.title}${i}`}>
+            <ImageCard
+              bg={value.bg}
+              img={value.img}
+              imgSize={100}
+              title={value.title}
+              learn
+              cardHeight="inherit"
+            />
+          </SwiperSlide>
+        ))
+      }
+    </Box>
+  )
+}
+
+export default FullScreenCarousel;
