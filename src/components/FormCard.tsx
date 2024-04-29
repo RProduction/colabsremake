f4bcfd8a84ed882c6c40350bfdf942a5c1b1d7fb
RProduction <starfallproduction42@gmail.com>
2024-04-29 08:40:11 +0700

    implement form card

---
+++ src/components/FormCard.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import CurvedContainer from './CurvedContainer';
+import Typography from '@mui/material/Typography';
+import Fab from '@mui/material/Fab';
+import InputBase from '@mui/material/InputBase';
+import Box from '@mui/material/Box';
+
+import Arrow from '@mui/icons-material/ArrowRight';
+
+
+interface Props {
+  title: string;
+  inputText: string;
+  caption: string;
+  description: string;
+  bg: string;
+  cardHeight: number;
+}
+
+function FormCard(props: Props) {
+  return(
+    <CurvedContainer bg={props.bg} padding={2} minHeight={props.cardHeight}>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        flexDirection="column"
+        height="100%"
+        gap={1}
+      >
+        <Typography color="white" variant="h4">
+          {
+            props.title.split("<br/>").map((value, i, arr) => (
+              <span>{value}{i < arr.length ? <br/> : null}</span>
+            ))
+          }
+        </Typography>
+        <Box
+          bgcolor="white"
+          borderRadius={10}
+          padding={2}
+        >
+          <InputBase
+            placeholder={props.inputText}
+            
+          />
+        </Box>
+        
+        <Box display="flex" justifyContent="right" alignItems="center" gap={1}>
+          <Typography color="white">
+            {props.caption}
+          </Typography>
+          <Fab
+            color='black'
+            sx={{
+              color: "white",
+            }}
+          >
+            <Arrow/>
+          </Fab>
+        </Box>
+        <Typography color="white">
+          {props.description}
+        </Typography>
+      </Box>
+    </CurvedContainer>
+  )
+}
+
+export default FormCard;
